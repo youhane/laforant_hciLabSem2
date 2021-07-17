e93837dbd8ea7b50465b26616f65a2762fb9fba9
youhane <jason.sebastian154@gmail.com>
2021-07-17 22:14:41 +0700

    i'm stuck

--- script/sysreq.js
+++ script/sysreq.js
@@ -11,6 +11,52 @@ const navLinks = {
         }
     }
 }
-
-
 Vue.createApp(navLinks).mount("#nav-links")
+
+const minimal = {
+    data() {
+        return {
+            minimal: [
+                { detail: "Windows 7 64 bit" },
+                { detail: "Intel Core i3" },
+                { detail: "8 GB RAM" },
+                { detail: "12GB" },
+                { detail: "Nwidia MX750" },
+                { detail: "Version 11" )
+            ]
+        }
+    }
+}
+Vue.createApp(minimal).mount("#minimal")
+
+const recommended = {
+    data() {
+        return {
+            recommended: [
+                { detail: "Windows 7 64 bit" },
+                { detail: "Intel Core i7" },
+                { detail: "16 GB RAM" },
+                { detail: "30GB" },
+                { detail: "Nwidia RTX2021" },
+                { detail: "Version 20" )
+            ]
+        }
+    }
+}
+Vue.createApp(recommended).mount("#recommended")
+
+const specs = {
+    data() {
+        return {
+            minimal: [
+                { detail: "OS" },
+                { detail: "Processor" },
+                { detail: "Memory" },
+                { detail: "Storage" },
+                { detail: "Graphics Card" },
+                { detail: "DirectX" )
+            ]
+        }
+    }
+}
+Vue.createApp(specs).mount("#specs")
